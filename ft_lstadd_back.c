87c0ebdbbf0d9c203d5bf1dcec87a18c43726bae
Francine Eli Barbosa <feli-bar@c1r10p1.42sp.org.br>
2022-09-22 16:22:04 -0300

    Adicionando exercicios bonus corrigidos

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: feli-bar <feli-bar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 13:42:39 by feli-bar          #+#    #+#             */
-/*   Updated: 2022/09/22 14:56:59 by feli-bar         ###   ########.fr       */
+/*   Updated: 2022/09/22 16:18:48 by feli-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	*lst = ft_lstlast(*lst);
-	lst->next = new;
+	t_list	*backnode;
+
+	if (new == NULL)
+		return ;
+	else if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		backnode = ft_lstlast(*lst);
+		backnode->next = new;
+	}
 }
